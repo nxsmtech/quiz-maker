b7bc233925e0bb231813d9e65e0e8ea5f41eb3a1
nikita <nx.smtech@gmail.com>
2024-11-25 18:23:57 +0200

    Return null if poll is inactive

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -7,8 +7,12 @@ use Illuminate\View\View;
 
 class PollController extends Controller
 {
-    public function show(Poll $poll): View
+    public function show(Poll $poll): ?View
     {
+        if (!$poll->is_active) {
+            return null;
+        }
+
         return view('components.poll-preview', ['poll' => $poll]);
     }
 }
