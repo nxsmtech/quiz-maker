ad2aa6979734eef5353bea16b011464c1c6d5da4
nikita <nx.smtech@gmail.com>
2024-12-03 17:43:46 +0200

    Add csrf token and access-control headers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+        header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, X-CSRF-TOKEN');
     }
 }
