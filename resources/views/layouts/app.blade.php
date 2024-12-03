222937551a74d3ccba1f6fda2444b5186ee03c37
nikita <nx.smtech@gmail.com>
2024-12-03 18:01:47 +0200

    Add middleware rule

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Filament + Livewire</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
